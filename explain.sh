08c31594e347b65cf6fe93ff51ba73a9eaf1adba
BerkayYavuz <mberkayyavuz@hotmail.com>
2025-07-24 17:13:06 +0300

    updated.

--- explain.sh
+++ explain.sh
@@ -3,6 +3,10 @@
 if [ "$#" -eq 0 ]
 then
 	directory=$(pwd)
+else
+	directory=$1
+fi
+
 echo "Dizindeki dosyalar, boyutları ve toplam boyutu :"
 du -sch * $directory #dosyaların boyutunu gösterir
 echo "Dizindeki dosya yapısını gösterir :"
@@ -23,30 +27,4 @@ stat -c %G $directory
 echo "Dizindeki dosyaların uzantı listesi :"
 #dosya sayısı 10 ile sınırlandırıldı.
 find . $directory -type f -name "*" | sort -r | head -n 10
-
-else
-	directory=$1
-	echo "Dizindeki dosyalar ve boyutları :"
-du -sch * $directory #dosyaların boyutunu gösterir
-echo "Dizindeki dosya yapısını gösterir :"
-ls -A  $directory #dosya yapısını gösterir
-echo "Dizindeki en büyük boyutlu dosya :"
-du -ah $directory | sort -nr | head -n 1 #en büyük dosyayı gösterir
-#du -sh
-echo "Dizindeki en küçük boyutlu dosya :"
-du -ah  $directory |sort -h | head -n 1 #en küçük dosyayı gösterir
-echo "Dizindeki dosya sayısı :"
-find . $directory | -type d | wc -l
-echo "Dizindeki klasör sayısı :"
-find . $directory -type f | wc -l
-echo "Dizinin sahibi :"
-stat -c  %U $directory
-echo "Dizinin sahibinin grubu :"
-stat -c %G $directory
-echo "Dizindeki dosyaların uzantı listesi:"
-#dosya sayısı 10 ile sınırlandırıldı.
-find . $directory -type f -name "*" | sort -r | head -n 10
-
-
-fi
 
